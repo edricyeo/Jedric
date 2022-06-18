f04ddcca1639519f5daa8bdcae72d46a5b29debe
Edric Yeo <edricyyj@gmail.com>
2022-06-18 20:58:57 +0800

    fixed naming of scenes

---
+++ Jedric/Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    private bool opened;
+    [SerializeField] private GameObject portalClosed;
+    [SerializeField] private GameObject portalOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opened = false;
+        portalClosed.SetActive(true);
+        portalOpen.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && opened == true)
+        {
+            // player runs to portal and boss is dead, go to next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void OpenPortal()
+    {
+        portalClosed.SetActive(false);
+        portalOpen.SetActive(true);
+        opened = true;
+    }
+
+    
+}
