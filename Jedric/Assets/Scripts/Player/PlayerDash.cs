c5d243a7e3354427a30509681431ffaa17d189c1
Edric Yeo <edricyyj@gmail.com>
2022-06-23 00:46:28 +0800

    implemented playerDash, implemented DontDestroyOnLoad for player

---
+++ Jedric/Assets/Scripts/Player/PlayerDash.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMove;
+
+    [SerializeField] private float dashMultiplier;
+    private bool dashEnabled = false;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashCooldown;
+    private float dashDurationTimer;
+    private float dashCdTimer;
+    public bool isDashing = false;
+
+    private void Start()
+    {
+        Health.BossDeathEvent += ToggleDash;
+    }
+
+    void Update()
+    {
+        if (dashEnabled == true)
+        {
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                if (dashCdTimer <= 0 && dashDurationTimer <= 0)
+                {
+                    playerMove.speed *= dashMultiplier;
+                    isDashing = true;
+                    dashDurationTimer = dashDuration;
+                }
+            }
+
+            if (dashDurationTimer > 0)
+            {
+                // player is dashing 
+                dashDurationTimer -= Time.deltaTime;
+
+                if (dashDurationTimer <= 0)
+                {
+                    // player stops dashing, set move speed back to normal
+                    float originalSpeed = playerMove.speed /= dashMultiplier;
+                    
+                  
+                    playerMove.speed = originalSpeed;
+                    dashCdTimer = dashCooldown;
+
+                    isDashing = false;
+                }
+            }
+            
+            if (dashCdTimer > 0)
+            {
+                // dash is on cooldown before it can be used again
+                dashCdTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    void ToggleDash()
+    {
+        if (dashEnabled == false)
+        {
+            dashEnabled = true;
+        }
+    }
+}
