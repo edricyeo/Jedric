e172332d931cac6980982e1f8e8e5fbce50551a0
Jed Ng <jednghk@gmail.com>
2022-06-27 01:52:32 +0800

    Experimented with health script

--- Jedric/Assets/Scripts/Boss2/Boss2.cs
+++ Jedric/Assets/Scripts/Boss2/Boss2.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class Boss2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+
+    [Header("Ranged Attack")]
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private GameObject[] bolts;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    //References
+    private Animator anim;
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
+        if (PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("attack02");
+            }
+        }
+    }
+
+    private void RangedAttack()
+    {
+        cooldownTimer = 0;
+        bolts[FindBolt()].transform.position = firepoint.position;
+        bolts[FindBolt()].GetComponent<EnemyProjectile>().ActivateProjectile();
+    }
+    private int FindBolt()
+    {
+        for (int i = 0; i < bolts.Length; i++)
+        {
+            if (!bolts[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit =
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
--- Jedric/Assets/Scripts/Boss2/EnemyProjectile.cs
+++ Jedric/Assets/Scripts/Boss2/EnemyProjectile.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float resetTime;
+    private float lifetime;
+    private Animator anim;
+    private BoxCollider2D coll;
+
+    private bool hit;
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateProjectile()
     {
-        
+        hit = false;
+        lifetime = 0;
+        gameObject.SetActive(true);
+        coll.enabled = true;
+    }
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > resetTime)
+            gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        coll.enabled = false;
+
+        if (collision.CompareTag("Enemy"))
+            collision.GetComponent<Health>().TakeDamage(1);
+
+        Deactivate(); //When this hits any object deactivate arrow
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
